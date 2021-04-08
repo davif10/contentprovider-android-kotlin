5342877115afb0509ceac0e927abe5aeb8d9e47c
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-04-08 17:40:41 -0300

    Update Project
    Update Project

---
+++ ContentProvider/app/src/main/java/com/davisilvaprojetos/contentprovider/NoteClickedListener.kt
@@ -0,0 +1,9 @@
+package com.davisilvaprojetos.contentprovider
+
+import android.database.Cursor
+
+interface NoteClickedListener {
+
+    fun noteClickedItem(cursor: Cursor)
+    fun noteRemoveItem(cursor: Cursor?)
+}
